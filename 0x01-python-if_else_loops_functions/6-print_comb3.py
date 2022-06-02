fd23b298a557d1308f409434ed75e1940bb9ce77
Joshua Maina <josh15maina@gmail.com>
2022-06-02 03:23:28 +0000

    Sake

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for a in range(0, 8):
-    for b in range(a + 1, 10):
-        print("{:d}{:d}, ".format(a, b), end=', ')
-print("{:d}{:d}".format(a + 1, b))
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
+
