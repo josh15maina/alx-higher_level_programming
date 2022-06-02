61611b9e1796cd71b4ca5ad880983229b9c215e9
Joshua Maina <josh15maina@gmail.com>
2022-06-02 03:11:20 +0000

    Dru

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for c in range(0, 99):
-    print("{:02d}".format(c), end=',')
+    print("{:02d}".format(c), end=', ')
 else:
     print("{:02d}".format(c + 1))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for a in range(0, 8):
     for b in range(1, 10):
-        print("{:d}{:d}, ".format(a, b), end=",")
+        print("{:d}{:d}, ".format(a, b), end=', ')
 print("{:d}{:d}".format(a + 1, b))
