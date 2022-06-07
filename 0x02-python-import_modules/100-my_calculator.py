02c9de0f55bb991b870ef8227254251e2f35f86a
josh15maina <josh15maina@gmail.com>
2022-06-07 21:25:05 +0000

    Toast

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-     argc = len(argv)
+    argc = len(argv)
     if (argc != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
