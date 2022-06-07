8366a0f11869ca1c326571fefc1a05d1a06d5ea0
josh15maina <josh15maina@gmail.com>
2022-06-07 18:20:27 +0000

    Truble

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    slist = list(my_string)
-    [slist.remove(i) for i in slist if i == 'c' or i == 'C']
-    return("".join(slist))
+    new_string = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
+    return new_string
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if my_list:
+	my_list.sort()
+	return my_list[-1]
+    else:
         return (None)
-    max = sorted(my_list)[-1]
-    return (max)
