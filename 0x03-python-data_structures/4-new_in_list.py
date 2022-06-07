14a7d8c7127f88d768c8339c5f6635384ae28784
josh15maina <josh15maina@gmail.com>
2022-06-07 14:39:09 +0000

    Checker stress

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-	return (my_list)
-    new_lst = my_list[:]
-    new_lst[idx] = element
-    return (new_lst)
-
+    new_list = my_list[:]
+    if idx < 0 or idx < (len(new_list - 1):
+        new_list[idx] = element
+        return (new_list)
+    return (my_list)
