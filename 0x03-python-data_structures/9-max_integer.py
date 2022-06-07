91a986ef097f09628914b200bfdcf7ec1df98bb4
josh15maina <josh15maina@gmail.com>
2022-06-07 19:12:06 +0000

    Flags

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-	my_list.sort()
-	return my_list[-1]
-    else:
+    if len(my_list) == 0:
         return (None)
+    max = sorted(my_list)[-1]
+    return (max)
