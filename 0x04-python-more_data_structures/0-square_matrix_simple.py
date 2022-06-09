71255ff18a264a4968cebe974c196c6fd8794cfc
Joshua Maina <josh15maina@gmail.com>
2022-06-09 17:17:36 +0000

    Wheeezy

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[y **  for y in z] for z in matrix]
+    new_matrix = [[y ** 2 for y in z] for z in matrix]
     return (new_matrix)
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = " "
-    for i in range(0, len(my_list)):
-        if (mylist[i] == "search"):
-            new_list += "replace"
-        else:
-            new_list += my_list[i]
-        return (new_list)
+    return list(map(lambda z: replace if z == search else z, my_list))
