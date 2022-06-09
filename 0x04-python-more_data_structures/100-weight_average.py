28d1106682aa7b16c2989879e65103a6badab4f5
Joshua Maina <josh15maina@gmail.com>
2022-06-09 21:37:52 +0000

    Drummer

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    weighted_sum = []
+    for a, b in zip(score, weight):
+        weighted_sum.append(score * weight)
+
+    return round(sum(weighted_sum) / sum(weight))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if roman_string is None or type(roman_string) is not str:
         return (0)
-    Rm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    rm = roman_string
-    i_list = [Rm[i[0]] if Rm[i[0]] >= Rm[i[1]] else (-1*Rm[i[0]])
-               for i in zip(rm, rm[1:] + rm[-1])]
-    dec = sum(i_list)
-    return (dec)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            integer += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+        else:
+            integer += roman[roman_string[i]]
+    return (integer)
