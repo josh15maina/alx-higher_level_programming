41b9516e7f57c5e2590cb8f31cdb581cbcda2042
Joshua Maina <josh15maina@gmail.com>
2022-06-09 19:23:32 +0000

    Big Mood

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary((x, y*2) for x, y in a_dictionary.items())
+    new_dict = a_dictionary.update({n: 2 * a_dictionary[n] for n in a_dictionary.items()})
     return (new_dict)
